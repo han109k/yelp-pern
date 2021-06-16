3d99a6ccc240d5c6ce495179d8f8e7f336b70fb0
mkx <han109k@gmail.com>
2021-06-16 12:50:26 +0300

    baseURL fix

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -4,7 +4,10 @@ import axios from "axios";
 // If in production baseURL = /api/v1/restaurants
 // else baseURL = http://localhost:5000/api/v1/restaurants 
 
-const baseURL = process.env.NODE_ENV === 'production' ? "api/v1/restaurants" : "http://localhost:5000/api/v1/restaurants";
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "/api/v1/restaurants"
+    : "http://localhost:5000/api/v1/restaurants";
 
 export default axios.create({
     baseURL    // baseURL: baseURL;
